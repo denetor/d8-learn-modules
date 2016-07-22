9d16369f2f627d8765199b6d7d280d44f8a02480
Nicola Da Rold <denetor@gmail.com>
2016-07-22 23:11:16 +0200

    Aggiunto form di configurazione del blocco

--- hello_world/src/Controller/HelloController.php
+++ hello_world/src/Controller/HelloController.php
@@ -1,17 +1,28 @@
 <?php
+
 /**
  * @file
  * Contains \Drupal\hello_world\Controller\HelloController.
  */
+
 namespace Drupal\hello_world\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
 class HelloController extends ControllerBase {
-  public function content() {
-    return array(
-        '#type' => 'markup',
-        '#markup' => $this->t('Ciao mondo!'),
-    );
-  }
+
+    
+    
+    public function content() {
+        return array(
+            '#type' => 'markup',
+            '#markup' => $this->t('Ciao mondo, dal controller di Hello', array(
+                '@name' => $name,
+                    )
+            ),
+        );
+    }
+
+    
+    
 }
