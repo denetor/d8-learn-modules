2ad751e60587f7eeacbb85316c3e7dba49c93f4c
Nicola Da Rold <denetor@gmail.com>
2016-07-22 22:35:26 +0200

    Aggiunto blocco per il modulo

---
+++ hello_world/src/Plugin/Block/HelloBlock.php
@@ -0,0 +1,23 @@
+<?php
+namespace Drupal\hello_world\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block
+ *
+ * @Block(
+ *   id = "hello_block",
+ *   admin_label = @Translation("Hello block"),
+ * )
+ */
+class HelloBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#markup' => $this->t('Ciao mondo, sono un blocco'),
+    );
+  }
+}
